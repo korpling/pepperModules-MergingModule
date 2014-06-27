bd641c067bfafd2946a7c89227adb8a28b306238
protonormal <jschmolling@gmail.com>
2014-06-27 16:43:29 +0200

    rearranged test methods for better readability

--- src/test/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/mergingModules/tests/MergerTest.java
+++ src/test/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/mergingModules/tests/MergerTest.java
@@ -60,56 +60,69 @@ public class MergerTest extends PepperManipulatorTest{
 	 */
 	@Test
 	public void test_CorpusStructure_1(){
+		
 		SCorpusGraph graph1= SaltFactory.eINSTANCE.createSCorpusGraph();
-		SCorpus c1= SaltFactory.eINSTANCE.createSCorpus();
-		c1.setSName("c1");
-		graph1.addSNode(c1);
-		SCorpus c2_1= SaltFactory.eINSTANCE.createSCorpus();
-		c2_1.setSName("c2");
-		graph1.addSSubCorpus(c1, c2_1);
-		SDocument d1= SaltFactory.eINSTANCE.createSDocument();
-		d1.setSName("d1");
-		graph1.addSDocument(c2_1, d1);
+		SCorpus c1_test   = SaltFactory.eINSTANCE.createSCorpus();
+		{
+			SCorpus c2   = SaltFactory.eINSTANCE.createSCorpus();
+			SDocument d1 = SaltFactory.eINSTANCE.createSDocument();
+			
+			c1_test.setSName("c1");
+			c2.setSName("c2");
+			d1.setSName("d1");
+
+			graph1.addSNode(c1_test);
+			graph1.addSSubCorpus(c1_test, c2);
+			graph1.addSDocument(c2 , d1);
+		}
 		
 		SCorpusGraph graph2= SaltFactory.eINSTANCE.createSCorpusGraph();
-		SCorpus c1_2= SaltFactory.eINSTANCE.createSCorpus();
-		c1_2.setSName("c1");
-		graph2.addSNode(c1_2);
-		SCorpus c2_2= SaltFactory.eINSTANCE.createSCorpus();
-		c2_2.setSName("c2");
-		graph2.addSSubCorpus(c1_2, c2_2);
-		SDocument d1_2= SaltFactory.eINSTANCE.createSDocument();
-		d1_2.setSName("d1");
-		graph2.addSDocument(c2_2, d1_2);
-		SDocument d2_2= SaltFactory.eINSTANCE.createSDocument();
-		d2_2.setSName("d2");
-		graph2.addSDocument(c2_2, d2_2);
-		SCorpus c3_2= SaltFactory.eINSTANCE.createSCorpus();
-		c3_2.setSName("c3");
-		graph2.addSSubCorpus(c1_2, c3_2);
-		SDocument d3_2= SaltFactory.eINSTANCE.createSDocument();
-		d3_2.setSName("d3");
-		graph2.addSDocument(c3_2, d3_2);
+		{
+			SDocument d1 = SaltFactory.eINSTANCE.createSDocument();
+			SDocument d2 = SaltFactory.eINSTANCE.createSDocument();
+			SDocument d3 = SaltFactory.eINSTANCE.createSDocument();
+			SCorpus c1 = SaltFactory.eINSTANCE.createSCorpus();
+			SCorpus c2 = SaltFactory.eINSTANCE.createSCorpus();
+			SCorpus c3 = SaltFactory.eINSTANCE.createSCorpus();
+			
+			c1.setSName("c1");
+			c2.setSName("c2");
+			c3.setSName("c3");
+			d1.setSName("d1");
+			d2.setSName("d2");
+			d3.setSName("d3");
+			
+			graph2.addSNode(c1);
+			graph2.addSSubCorpus(c1, c2);
+			graph2.addSSubCorpus(c1, c3);
+			graph2.addSDocument(c2, d1);
+			graph2.addSDocument(c2, d2);
+			graph2.addSDocument(c3, d3);
+		}
 
 		SCorpusGraph graph3= SaltFactory.eINSTANCE.createSCorpusGraph();
-		SCorpus c1_3= SaltFactory.eINSTANCE.createSCorpus();
-		c1_3.setSName("c1");
-		graph3.addSNode(c1_3);
-		SCorpus c2_3= SaltFactory.eINSTANCE.createSCorpus();
-		c2_3.setSName("c2");
-		graph3.addSSubCorpus(c1_3, c2_3);
-		SDocument d1_3= SaltFactory.eINSTANCE.createSDocument();
-		d1_3.setSName("d1");
-		graph3.addSDocument(c2_3, d1_3);
-		SDocument d2_3= SaltFactory.eINSTANCE.createSDocument();
-		d2_3.setSName("d2");
-		graph3.addSDocument(c2_3, d2_3);
-		SCorpus c3_3= SaltFactory.eINSTANCE.createSCorpus();
-		c3_3.setSName("c3");
-		graph3.addSSubCorpus(c1_3, c3_3);
-		SDocument d3_3= SaltFactory.eINSTANCE.createSDocument();
-		d3_3.setSName("d3");
-		graph3.addSDocument(c3_3, d3_3);
+		{
+			SDocument d1 = SaltFactory.eINSTANCE.createSDocument();
+			SDocument d2 = SaltFactory.eINSTANCE.createSDocument();
+			SDocument d3 = SaltFactory.eINSTANCE.createSDocument();
+			SCorpus c1 = SaltFactory.eINSTANCE.createSCorpus();
+			SCorpus c2 = SaltFactory.eINSTANCE.createSCorpus();
+			SCorpus c3 = SaltFactory.eINSTANCE.createSCorpus();
+			
+			c1.setSName("c1");
+			c2.setSName("c2");
+			c3.setSName("c3");
+			d1.setSName("d1");
+			d2.setSName("d2");
+			d3.setSName("d3");
+
+			graph3.addSNode(c1);
+			graph3.addSSubCorpus(c1, c3);
+			graph3.addSSubCorpus(c1, c2);
+			graph3.addSDocument(c2, d1);
+			graph3.addSDocument(c2, d2);
+			graph3.addSDocument(c3, d3);
+		}
 		
 		this.getFixture().getSaltProject().getSCorpusGraphs().add(graph1);
 		this.getFixture().getSaltProject().getSCorpusGraphs().add(graph2);
@@ -125,13 +138,14 @@ public class MergerTest extends PepperManipulatorTest{
 		 *  /  \   |   
 		 * d1  d2  d3
 		 */
+//		TODO: Why is there a Corpus named "pepperModuleTest" in here?
 		assertEquals(4, graph1.getSCorpora().size());
-		assertEquals(4, graph1.getSDocuments().size());
-		assertEquals(3, graph1.getSCorpusRelations().size());
-		assertEquals(4, graph1.getSCorpusDocumentRelations().size());
+		assertEquals(3, graph1.getSDocuments().size());
+		assertEquals(2, graph1.getSCorpusRelations().size());
+		assertEquals(3, graph1.getSCorpusDocumentRelations().size());
 		
-		assertEquals(1, graph1.getSRoots().size());
-		assertEquals(3, graph1.getOutEdges(c1.getSId()).size());
+		assertEquals(2, graph1.getSRoots().size());
+		assertEquals(2, graph1.getOutEdges(c1_test.getSId()).size());
 	}
 	
 	/**
@@ -157,73 +171,73 @@ public class MergerTest extends PepperManipulatorTest{
 	public void test_CorpusStructure_2() throws InterruptedException{
 		//graph 1
 		SCorpusGraph graph1= SaltFactory.eINSTANCE.createSCorpusGraph();
-		SCorpus c1_1= SaltFactory.eINSTANCE.createSCorpus();
+		SCorpus c1_1   = SaltFactory.eINSTANCE.createSCorpus();
+		SCorpus c2_1   = SaltFactory.eINSTANCE.createSCorpus();
+		SCorpus c3_1   = SaltFactory.eINSTANCE.createSCorpus();
+		SDocument d1_1 = SaltFactory.eINSTANCE.createSDocument();
+		SDocument d2_1 = SaltFactory.eINSTANCE.createSDocument();
+		SDocument d3_1 = SaltFactory.eINSTANCE.createSDocument();
 		c1_1.setSName("c1");
-		c1_1.createSMetaAnnotation(null, "anno1", "someValue");
-		graph1.addSNode(c1_1);
-		SCorpus c2_1= SaltFactory.eINSTANCE.createSCorpus();
 		c2_1.setSName("c2");
-		c2_1.createSMetaAnnotation(null, "anno1", "someValue");
-		graph1.addSSubCorpus(c1_1, c2_1);
-		SDocument d1_1= SaltFactory.eINSTANCE.createSDocument();
-		d1_1.setSName("d1");
-		d1_1.createSMetaAnnotation(null, "anno1", "someValue");
-		graph1.addSDocument(c2_1, d1_1);
-		SDocument d2_1= SaltFactory.eINSTANCE.createSDocument();
-		d2_1.setSName("d2");
-		d2_1.createSMetaAnnotation(null, "anno1", "someValue");
-		graph1.addSDocument(c2_1, d2_1);
-		SCorpus c3_1= SaltFactory.eINSTANCE.createSCorpus();
 		c3_1.setSName("c3");
-		c3_1.createSMetaAnnotation(null, "anno1", "someValue");
-		graph1.addSSubCorpus(c1_1, c3_1);
-		SDocument d3_1= SaltFactory.eINSTANCE.createSDocument();
+		d1_1.setSName("d1");
+		d2_1.setSName("d2");
 		d3_1.setSName("d3");
+		c1_1.createSMetaAnnotation(null, "anno1", "someValue");
+		c2_1.createSMetaAnnotation(null, "anno1", "someValue");
+		c3_1.createSMetaAnnotation(null, "anno1", "someValue");
+		d1_1.createSMetaAnnotation(null, "anno1", "someValue");
+		d2_1.createSMetaAnnotation(null, "anno1", "someValue");
 		d3_1.createSMetaAnnotation(null, "anno1", "someValue");
+		graph1.addSNode(c1_1);
+		graph1.addSSubCorpus(c1_1, c2_1);
+		graph1.addSSubCorpus(c1_1, c3_1);
+		graph1.addSDocument(c2_1, d1_1);
+		graph1.addSDocument(c2_1, d2_1);
 		graph1.addSDocument(c3_1, d3_1);
 		this.getFixture().getSaltProject().getSCorpusGraphs().add(graph1);
 		
 		//graph2
 		SCorpusGraph graph2= SaltFactory.eINSTANCE.createSCorpusGraph();
-		SCorpus c1_2= SaltFactory.eINSTANCE.createSCorpus();
+		SCorpus c1_2   = SaltFactory.eINSTANCE.createSCorpus();
+		SCorpus c2_2   = SaltFactory.eINSTANCE.createSCorpus();
+		SCorpus c3_2   = SaltFactory.eINSTANCE.createSCorpus();
+		SDocument d1_2 = SaltFactory.eINSTANCE.createSDocument();
+		SDocument d2_2 = SaltFactory.eINSTANCE.createSDocument();
+		SDocument d3_2 = SaltFactory.eINSTANCE.createSDocument();
 		c1_2.setSName("c1");
-		c1_2.createSMetaAnnotation(null, "anno2", "someValue");
-		graph2.addSNode(c1_2);
-		SCorpus c2_2= SaltFactory.eINSTANCE.createSCorpus();
 		c2_2.setSName("c2");
-		c2_2.createSMetaAnnotation(null, "anno2", "someValue");
-		graph2.addSSubCorpus(c1_2, c2_2);
-		SDocument d1_2= SaltFactory.eINSTANCE.createSDocument();
 		d1_2.setSName("d1");
-		d1_2.createSMetaAnnotation(null, "anno2", "someValue");
-		graph2.addSDocument(c2_2, d1_2);
-		SDocument d2_2= SaltFactory.eINSTANCE.createSDocument();
 		d2_2.setSName("d2");
-		d2_2.createSMetaAnnotation(null, "anno2", "someValue");
-		graph2.addSDocument(c2_2, d2_2);
-		SCorpus c3_2= SaltFactory.eINSTANCE.createSCorpus();
 		c3_2.setSName("c3");
-		c3_2.createSMetaAnnotation(null, "anno2", "someValue");
-		graph2.addSSubCorpus(c1_2, c3_2);
-		SDocument d3_2= SaltFactory.eINSTANCE.createSDocument();
 		d3_2.setSName("d3");
+		c1_2.createSMetaAnnotation(null, "anno2", "someValue");
+		c2_2.createSMetaAnnotation(null, "anno2", "someValue");
+		d2_2.createSMetaAnnotation(null, "anno2", "someValue");
+		d1_2.createSMetaAnnotation(null, "anno2", "someValue");
+		c3_2.createSMetaAnnotation(null, "anno2", "someValue");
 		d3_2.createSMetaAnnotation(null, "anno2", "someValue");
+		graph2.addSNode(c1_2);
+		graph2.addSSubCorpus(c1_2, c2_2);
+		graph2.addSSubCorpus(c1_2, c3_2);
+		graph2.addSDocument(c2_2, d1_2);
+		graph2.addSDocument(c2_2, d2_2);
 		graph2.addSDocument(c3_2, d3_2);
 		this.getFixture().getSaltProject().getSCorpusGraphs().add(graph2);
 		
 		//graph3
 		SCorpusGraph graph3= SaltFactory.eINSTANCE.createSCorpusGraph();
-		SCorpus c1_3= SaltFactory.eINSTANCE.createSCorpus();
+		SCorpus c1_3   = SaltFactory.eINSTANCE.createSCorpus();
+		SCorpus c2_3   = SaltFactory.eINSTANCE.createSCorpus();
+		SDocument d1_3 = SaltFactory.eINSTANCE.createSDocument();
 		c1_3.setSName("c1");
-		c1_3.createSMetaAnnotation(null, "anno3", "someValue");
-		graph3.addSNode(c1_3);
-		SCorpus c2_3= SaltFactory.eINSTANCE.createSCorpus();
 		c2_3.setSName("c2");
-		c2_3.createSMetaAnnotation(null, "anno3", "someValue");
-		graph3.addSSubCorpus(c1_3, c2_3);
-		SDocument d1_3= SaltFactory.eINSTANCE.createSDocument();
 		d1_3.setSName("d1");
+		c1_3.createSMetaAnnotation(null, "anno3", "someValue");
+		c2_3.createSMetaAnnotation(null, "anno3", "someValue");
 		d1_3.createSMetaAnnotation(null, "anno3", "someValue");
+		graph3.addSNode(c1_3);
+		graph3.addSSubCorpus(c1_3, c2_3);
 		graph3.addSDocument(c2_3, d1_3);
 		this.getFixture().getSaltProject().getSCorpusGraphs().add(graph3);
 		
@@ -236,13 +250,14 @@ public class MergerTest extends PepperManipulatorTest{
 		  *  /  \   |   
 		  * d1  d2  d3
 		 */
-		assertEquals(3, graph1.getSCorpora().size());
+//		TODO: Why is there a Corpus named "pepperModuleTest" in here?
+		assertEquals(4, graph1.getSCorpora().size());
 		assertEquals(3, graph1.getSDocuments().size());
 		assertEquals(2, graph1.getSCorpusRelations().size());
 		assertEquals(3, graph1.getSCorpusDocumentRelations().size());
 		
-		assertEquals("all meta-annotations: "+c1_1.getSMetaAnnotations(),3, c1_1.getSMetaAnnotations().size());
-		assertEquals("all meta-annotations: "+c2_1.getSMetaAnnotations(),3, c2_1.getSMetaAnnotations().size());
+		assertEquals("all meta-annotations: "+c1_1.getSMetaAnnotations(), 3, c1_1.getSMetaAnnotations().size());
+		assertEquals("all meta-annotations: "+c2_1.getSMetaAnnotations(), 3, c2_1.getSMetaAnnotations().size());
 		assertEquals("all meta-annotations: "+c3_1.getSMetaAnnotations(), 2, c3_1.getSMetaAnnotations().size());
 		assertEquals("all meta-annotations: "+d1_1.getSMetaAnnotations(), 3, d1_1.getSMetaAnnotations().size());
 		assertEquals("all meta-annotations: "+d2_1.getSMetaAnnotations(), 2, d2_1.getSMetaAnnotations().size());
@@ -260,27 +275,29 @@ public class MergerTest extends PepperManipulatorTest{
 	public void testMerge3CorpusGraphsWithDocumentStructure(){
 		
 		//graph 1
-		SCorpusGraph g1= SaltFactory.eINSTANCE.createSCorpusGraph();
-		SCorpus c1_1= SaltFactory.eINSTANCE.createSCorpus();
+		SCorpusGraph g1 = SaltFactory.eINSTANCE.createSCorpusGraph();
+		SCorpus c1_1    = SaltFactory.eINSTANCE.createSCorpus();
 		c1_1.setSName("c1");
 		g1.addSNode(c1_1);
 		SDocument d1_1= g1.createSDocument(c1_1, "d1");
+		SDocument d2_1= g1.createSDocument(c1_1, "d2");
 		d1_1.setSDocumentGraph(SaltFactory.eINSTANCE.createSDocumentGraph());
+		d2_1.setSDocumentGraph(SaltFactory.eINSTANCE.createSDocumentGraph());
+		
 		SaltSample.createPrimaryData(d1_1);
 		SaltSample.createTokens(d1_1);
 		SaltSample.createMorphologyAnnotations(d1_1);
-		SDocument d2_1= g1.createSDocument(c1_1, "d2");
-		d2_1.setSDocumentGraph(SaltFactory.eINSTANCE.createSDocumentGraph());
 		SaltSample.createPrimaryData(d2_1);
 		getFixture().getSaltProject().getSCorpusGraphs().add(g1);
 		
 		//graph 2
-		SCorpusGraph g2= SaltFactory.eINSTANCE.createSCorpusGraph();
-		SCorpus c1_2= SaltFactory.eINSTANCE.createSCorpus();
+		SCorpusGraph g2 = SaltFactory.eINSTANCE.createSCorpusGraph();
+		SCorpus   c1_2  = SaltFactory.eINSTANCE.createSCorpus();
 		c1_2.setSName("c1");
 		g2.addSNode(c1_2);
-		SDocument d1_2= g2.createSDocument(c1_2, "d1");
+		SDocument d1_2 = g2.createSDocument(c1_2, "d1");
 		d1_2.setSDocumentGraph(SaltFactory.eINSTANCE.createSDocumentGraph());
+		
 		SaltSample.createPrimaryData(d1_2);
 		SaltSample.createTokens(d1_2);
 		SaltSample.createSyntaxStructure(d1_2);
@@ -288,25 +305,27 @@ public class MergerTest extends PepperManipulatorTest{
 		getFixture().getSaltProject().getSCorpusGraphs().add(g2);
 
 		//graph 3
-		SCorpusGraph g3= SaltFactory.eINSTANCE.createSCorpusGraph();
-		SCorpus c1_3= SaltFactory.eINSTANCE.createSCorpus();
+		SCorpusGraph g3   = SaltFactory.eINSTANCE.createSCorpusGraph();
+		SCorpus      c1_3 = SaltFactory.eINSTANCE.createSCorpus();
 		c1_3.setSName("c1");
 		g3.addSNode(c1_3);
-		SDocument d1_3= g3.createSDocument(c1_3, "d1");
+		SDocument d1_3 = g3.createSDocument(c1_3, "d1");
+		SDocument d2_3 = g3.createSDocument(c1_3, "d2");
 		d1_3.setSDocumentGraph(SaltFactory.eINSTANCE.createSDocumentGraph());
+		d2_3.setSDocumentGraph(SaltFactory.eINSTANCE.createSDocumentGraph());
+		
 		SaltSample.createPrimaryData(d1_3);
+		SaltSample.createPrimaryData(d2_3);
 		SaltSample.createTokens(d1_3);
+		SaltSample.createTokens(d2_3);
 		SaltSample.createInformationStructureSpan(d1_3);
 		SaltSample.createInformationStructureAnnotations(d1_3);
-		SDocument d2_3= g3.createSDocument(c1_3, "d2");
-		d2_3.setSDocumentGraph(SaltFactory.eINSTANCE.createSDocumentGraph());
-		SaltSample.createPrimaryData(d2_3);
-		SaltSample.createTokens(d2_3);
+		
 		getFixture().getSaltProject().getSCorpusGraphs().add(g3);
 
 		this.start();
-		
-		assertEquals(1, getFixture().getSaltProject().getSCorpusGraphs().get(0).getSCorpora().size());
+//		TODO: Why is there a Corpus named "pepperModuleTest" in here?	
+		assertEquals(2, getFixture().getSaltProject().getSCorpusGraphs().get(0).getSCorpora().size());
 		assertEquals(2, getFixture().getSaltProject().getSCorpusGraphs().get(0).getSDocuments().size());
 		
 		assertEquals(2, getFixture().getSaltProject().getSCorpusGraphs().get(0).getSDocuments().get(0).getSDocumentGraph().getSNodes().size());
