0cbfeea751bdd1f3129c3576bfbc6ed19fc11460
korpling-Server <korpling@rz.hu-berlin.de>
2015-04-01 21:00:35 +0200

    jenkins build

--- src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/mergingModules/MergerMapper.java
+++ src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/mergingModules/MergerMapper.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2009 Humboldt-Universität zu Berlin, INRIA.
+ * Copyright 2015 Humboldt-Universität zu Berlin.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
  *
  *
  */
-
 package de.hu_berlin.german.korpling.saltnpepper.pepperModules.mergingModules;
 
 import java.util.ArrayList;
